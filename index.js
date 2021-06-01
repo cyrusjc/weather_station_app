08c983946ed6e4ef28cdefb1881b75c91c32e363
Cyrus <cyrus.cheung123@gmail.com>
2021-06-01 00:58:45 -0700

    post api from arduino

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
 const path = require('path')
+
+//middleware functions
+
 // Create the server
-const app = express()
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
   try {
@@ -16,7 +24,6 @@ app.get('/api/cow/:say', cors(), async (req, res, next) => {
 })
 // Serve our base route that returns a Hello World cow
 app.get('/api/cow/', cors(), async (req, res, next) => {
-  console.log("I got reached")
   try {
     const moo = cowsay.say({ text: 'Hello World!' })
     res.json({ moo })
@@ -29,3 +36,8 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Mixing it up on port ${PORT}`)
 })
+
+app.post('/', (req, res)=>{
+  console.log(req.body);
+  res.status(201).send('Successfull');
+});
