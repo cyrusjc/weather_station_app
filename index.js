32adeadb8e2595f2593d6713af3da1a7435544f7
Cyrus <cyrus.cheung123@gmail.com>
2021-06-03 22:07:03 -0700

    final heroku build

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-const path = require('path')
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
