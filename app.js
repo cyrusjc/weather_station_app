fae38c55e822cd6f63e3d88c1d6870890a52bb0a
Cyrus <cyrus.cheung123@gmail.com>
2021-06-06 05:01:04 -0700

    added sockets.io for react and backend to comms

---
+++ app.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const cors = require('cors');
+
+//========================================
+// Initializing Express
+//========================================
+const express = require('express');
+// backend declaration
+const PORT_BACKEND = process.env.PORT || 5000
+const app_backend = express();
+const index = require("./routes/index");
+
+//middleware functions
+app_backend.use(express.urlencoded({extended: false}));
+app_backend.use(express.json());
+
+const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require('constants');
+
+// // Serve static files from the React frontend app
+// app_backend.use(express.static(path.join(__dirname, 'client/build')))
+// // Anything that doesn't match the above, send back index.html
+// app_backend.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
+
+app_backend.use(index);
+
+app_backend.listen(PORT_BACKEND, () => {
+  console.log(`Mixing it up on port ${PORT_BACKEND}`)
+})
+
+
+//========================================
+// Initializing Mongoose=================
+//========================================
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+//Connecting to mongoDB using mongoose
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result)=> console.log('connected'))
+  .catch((err)=> console.log(err));
+
+//========================================
+// Initializing socket.io and http
+//========================================
+
+const app = express();
+const http = require("http");
+
+const socket_index = require("./routes/socket_index");
+app.use(socket_index);
+
+//port declaration
+const PORT_SOCKET = process.env.PORT || 4001;
+
+//socket.io declaratio
+const server = http.createServer(app);
+//const io = socketIo(server); 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }
+});
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  console.log(response);
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(PORT_SOCKET, () => {
+  console.log(`Listening on port ${PORT_SOCKET}`)
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,28 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
 
 
+const ENDPOINT = "http://localhost:4001";
 
-class App extends Component {
-  state = {
-    cow: '',
-    text: ''
-  }
-componentDidMount() {
-    this.fetchCow()
-  }
-fetchCow = async () => {
-    const response = await fetch(`/api/cow`)
-    const initialCow = await response.json()
-    const cow = initialCow.moo
-    this.setState({ cow })
-  }
-customCow = async evt => {
-    evt.preventDefault()
-    const text = this.state.text
-    const response = await fetch(`/api/cow/${text}`)
-    const custom = await response.json()
-    const cow = custom.moo
-    this.setState({ cow, text: '' })
-    console.log(response);
-  }
-handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-    console.log(this.state.text)
-  }
-getData = async evt =>{
-  const response = await fetch(`/api/getData`)
-  const custom = await response.json()
-  console.log(custom);
-  console.log(response);
+function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT,{
+      withCredentials: true,
+    });
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
+
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+  );
+
+  
 }
-render() {
-    return (
-      <div className="App">
-        <h3>Text Cow. Moo</h3>
-        <code>{this.state.cow}</code>
-        <form onSubmit={this.customCow}>
-          <label>Custom Cow Text:</label>
-          <input
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Show me a talking cow!</button>
-        </form>
-        <button type="submit" onClick={this.getData}>Get Data</button>
-      </div>
-    )
-  }
-}
+
 export default App
