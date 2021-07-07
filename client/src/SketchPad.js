b9df2172e17004c5e86306c8ee4726ef9620cb8d
icefalcn <icefalcn@gmail.com>
2021-07-07 15:49:16 -0700

    added minor styling to page and added grid to canvas

--- client/src/SketchPad.js
+++ client/src/SketchPad.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
+import Button from 'react-bootstrap/Button';
 import './SketchPad.css'
 
-const scale = 3;
+const scale = 5;
 const ENDPOINT = "http://localhost:4001";
 
 function SketchPad() {
@@ -12,41 +13,64 @@ function SketchPad() {
   const graphDiameter = 250;
   var [graphDataPoints, setGraphDataPoints] = useState([{ x: graphDiameter, y: graphDiameter }]);
   var [dataPoints, setDataPoints] = useState([graphDiameter, graphDiameter])
-
-
+  var heart = [
+    {x:250,y:250},{x:260,y:240},{x:270,y:230},{x:280,y:230},{x:290,y:240},{x:300,y:250},{x:300,y:260},
+    {x:290,y:270},{x:280,y:280},{x:270,y:290},{x:260,y:300},{x:250,y:310},
+    {x:240,y:300},{x:230,y:290},{x:220,y:280},{x:210,y:270},
+    {x:200,y:260},{x:200,y:250},{x:210,y:240},{x:220,y:230},{x:230,y:230},{x:240,y:240},{x:250,y:250}]
+  var grid = [];
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT, {
       withCredentials: true,
     });
     socket.on("FromAPI", data => {
-      
-      //console.log(data);
-      //test();
       processJoystickData(data);
-
     });
   }, [graphDataPoints]);
 
-  const test = ()=> {
+  function clearCanvas(){
+    var canvas = document.getElementById("sketchCanvas");
+    const context = canvas.getContext('2d');
+    context.beginPath();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+  }
 
+  function drawGrid(){      
     var c = document.getElementById("sketchCanvas");
     var ctx = c.getContext("2d");
-    ctx.moveTo(dataPoints[0],dataPoints[1]);
-    var dataArray = graphDataPoints;
-    var dataPoint = dataPoints;
+    ctx.beginPath();
+    ctx.strokeStyle="rgb(230, 255, 255)"
+    for(let i = 1; i< 250*2/5;i++){
+      ctx.moveTo(i*5, 0);
+      ctx.lineTo(i*5, i*250*2);
+      ctx.stroke();      
+      ctx.moveTo(0, i*5);
+      ctx.lineTo(i*250*2, i*5);
+      ctx.stroke();
+    }
+  }
+  
 
-    dataPoint[0] = dataPoint[0] - 1;
+  function drawHeart(){
+    drawFromArray(heart);
+  }
 
-    dataArray.push([dataPoint[0], dataPoint[1]]);
-    setGraphDataPoints(dataArray);
-    ctx.lineTo(dataPoint[0],dataPoint[1]);
-    ctx.stroke();
-    
-    // setDataPoints([dataPoints[0] - 1, dataPoints[1]]);
-    // graphDataPoints.push({x:dataPoints[0],y:dataPoints[1]})
-    // setGraphDataPoints(graphDataPoints);
-    console.log(graphDataPoints);
+  function drawFromArray(data){
+    for(let i =0;i<data.length-1; i++){
+      (function(index) {
+        setTimeout(function() {
+          var c = document.getElementById("sketchCanvas");
+          var ctx = c.getContext("2d");
+          ctx.beginPath();
+          ctx.strokeStyle="black"
+          ctx.moveTo(data[i].x,data[i].y);
+          ctx.lineTo(data[i+1].x,data[i+1].y);
+          ctx.stroke();
+         }, i * 100);      
+      })(i);
+    }
   }
 
   function processJoystickData(data) {
@@ -58,37 +82,48 @@ function SketchPad() {
 
     var dataArray = graphDataPoints;
     var dataPoint = dataPoints;
-
+    let changed = false;
     if (data.x_data < 50) {
-      dataPoint[0] = dataPoint[0] - scale;
+      dataPoint[0] = dataPoint[0] + scale;
+      changed = true;
     }
     else if (data.x_data > 90) {
-      dataPoint[0] = dataPoint[0] + scale;
+      dataPoint[0] = dataPoint[0] - scale;
+      changed = true;
     }
     if (data.y_data < 50) {
-      dataPoint[1] = dataPoint[1] - scale;
+      dataPoint[1] = dataPoint[1] + scale;
+      changed = true;
     }
     else if (data.y_data > 90) {
-      dataPoint[1] = dataPoint[1] + scale;
+      dataPoint[1] = dataPoint[1] - scale;
+      changed = true;
     }
-    dataArray.push([dataPoint[0], dataPoint[1]]);
-    setGraphDataPoints(dataArray);
-    console.log(dataPoint);
+    if(changed){
+      dataArray.push([dataPoint[0], dataPoint[1]]);
+      setGraphDataPoints(dataArray);
+      ctx.lineTo(dataPoint[0],dataPoint[1]);
+      ctx.stroke();
+    }
 
-    ctx.lineTo(dataPoint[0],dataPoint[1]);
-    ctx.stroke();
   }
   
   return (
     <div className="SketchPad">
       <div>
-        <h3>Online Etch-a-sketch</h3>
+        <h3 className = "sketchTitlte">Online Etch-a-sketch</h3>
         <canvas id="sketchCanvas" width={graphDiameter*2} height={graphDiameter*2}>
           Your browser does not support the HTML canvas tag.</canvas>
       </div>
-      <button onClick={() => test()}>
-        Click me
-      </button>
+      <Button variant="primary" onClick={() => drawHeart()}>
+        Draw Heart
+      </Button>{' '}
+      <Button variant="danger" onClick={() => clearCanvas()}>
+        Clear
+      </Button>{' '}
+      <footer class="text-muted text-center text-small">
+        <p class="mb-1">© 2021-2021 Cyrus Cheung & Michael Liu</p>
+      </footer>
     </div>
   );
 }
